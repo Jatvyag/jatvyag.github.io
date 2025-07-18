963ce352864a5d21057dc80b6de1fc6be9cbedb9
jatvyag <jatvyag@proton.me>
2025-07-18 22:52:45 +0300

    Fix styles

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import '@/styles/main.css'
+import '@/styles/main.scss'
 import App from '@/App.vue'
 import router from '@/router'
 import i18n from '@/i18n'
