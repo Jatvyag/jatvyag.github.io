9c7cc47c21ccd7aeb403039193e6574285a44a3c
Jatvyag <Jatvyag@users.noreply.github.com>
2025-07-20 14:53:37 +0000

    deploy: 7ffa01f375da3db2bb858b4a95c73ca4d6b20222

---
+++ assets/legal-sources-DWcayULj.js
@@ -0,0 +1,31 @@
+const e=`# Sources of legal scientific literature
+Students, scientists, and practicing lawyers are often faced with the need to search for legal scientific literature. Previously, a significant amount of this work was performed by librarians, leaving the reader time and energy to study the content of the material found. After the transition of publications to digital platforms, the situation has changed significantly. Now the reader is forced to independently assess the reliability of information sources among the entire volume of information available online.
+
+## Major publishers
+
+In this post, I’ll talk about the main sources of legal information from the major publishers and aggregators, as well as my experience using their products.
+
+[Google Academy](https://scholar.google.com/) indexes metadata[^1] of scholarly literature from journal sites and other online sites. The advantage of this product is the wide coverage of published sources. However, the search results include not only full-text articles, but also brief information about them.
+
+[ROAD](https://road.issn.org/) is created and maintained by the International Serials Registration Center. Given that all scientific periodicals must first obtain an International Serial Number (ISSN), their open access information is the most reliable. This resource can also be used to check if a publication exists.
+
+Listing in the [Directory of Open Journals](http://www.doaj.org/) is often the first step for a publisher on the way to other indexed databases. In connection with this, you can find quite rare, although authoritative editions, which is not uncommon among conservative representatives of jurisprudence.
+
+[Paperity](https://paperity.org/) is an aggregator of open access journals and research papers. Therefore, the same materials can be found in other online sites, but the convenience is the ability to use this aggregator as a single point of access to many publications.
+
+[Bepress Legal Repository](http://law.bepress.com/) is the largest full-text online legal repository with open access. Many English-language articles in licensed databases are listed here.
+
+Materials in [SSRN](http://www.ssrn.com/en) are posted by the authors themselves. In this regard, articles are published with a time lag of a year or two, which depends on the content of the license agreement with the original publishing house. At the same time, it is recommended to take part in the activity of the legal scientific subnet of the network of social sciences, which, in addition to the exchange of publications, allows you to get involved in the life of the international academic community.
+Places for publications
+
+I would like to note separately that some articles are publicly available in licensed databases. Such possibilities are presented here:
+- [ScienceDirect](http://www.sciencedirect.com/)
+- [Wiley Online Library](http://onlinelibrary.wiley.com/)
+- [JSTOR](http://www.jstor.org/)
+- [Ingenta Connect](http://www.ingentaconnect.com/)
+- [Taylor and Francis0](http://www.tandfonline.com/)
+- [ProQuest Ebook Central](https://ebookcentral.proquest.com/lib/nlbby/home.action)
+- [SAGE Journals](http://online.sagepub.com/)
+- [Springer](https://link.springer.com/)
+
+[^1]: Metadata is information about data (author, title, keywords, etc.) in HTML and tags. There are few metadata standards. The webmaster guidelines page lists the standards handled by Google Academy (Highwire Press, Eprints, BE Press, PRISM). Dublin Core tags are listed as least desirable because they are not suitable enough for academic journals.`;export{e as default};
